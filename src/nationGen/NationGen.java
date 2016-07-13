fad06768f76b2f2f45b0b1b29e1bfd0bcb5536fd
elmokki <elmokki@kapsi.fi>
2016-07-13 10:11:22 +0300

    Prepare for 0.6.15

--- src/nationGen/NationGen.java
+++ src/nationGen/NationGen.java
@@ -52,8 +52,8 @@ import nationGen.units.Unit;
 
 
 public class NationGen {
-	public static String version = "0.6.14c";
-	public static String date = "10th of July 2016";
+	public static String version = "0.6.15";
+	public static String date = "13th of July 2016";
 	
 	public List<NationRestriction> restrictions = new ArrayList<NationRestriction>();
 	
