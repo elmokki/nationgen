aa4953a005cdeff7d2f50e58c2f0203917beed13
elmokki <elmokki@kapsi.fi>
2016-08-23 18:57:04 +0300

    0.6.17c

--- src/nationGen/NationGen.java
+++ src/nationGen/NationGen.java
@@ -52,8 +52,8 @@ import nationGen.units.Unit;
 
 
 public class NationGen {
-	public static String version = "0.6.17b";
-	public static String date = "15th of August 2016";
+	public static String version = "0.6.17c";
+	public static String date = "23rd of August 2016";
 	
 	public List<NationRestriction> restrictions = new ArrayList<NationRestriction>();
 	
