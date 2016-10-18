7c1b29ddc968052a63571f04f50145b4dca016a9
Ealb <ea.buffer@gmail.com>
2016-10-18 18:57:31 -0400

    Changelog and new .jar

--- src/nationGen/NationGen.java
+++ src/nationGen/NationGen.java
@@ -53,8 +53,8 @@ import nationGen.units.Unit;
 
 
 public class NationGen {
-	public static String version = "0.6.18";
-	public static String date = "12th of September 2016";
+	public static String version = "0.6.19";
+	public static String date = "18th of October 2016";
 	
 	public List<NationRestriction> restrictions = new ArrayList<NationRestriction>();
 	
