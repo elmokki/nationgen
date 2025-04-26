dad3f7dca51b44f0a3a1fb756a3f2b505e0b4dfc
Drithyl <hugo.bujan@gmail.com>
2025-04-26 13:29:58 -0400

    v0.12.6

--- src/nationGen/NationGen.java
+++ src/nationGen/NationGen.java
@@ -33,8 +33,8 @@ import nationGen.units.Unit;
 
 public class NationGen {
 
-  public static String version = "0.12.5";
-  public static String date = "13th March 2025";
+  public static String version = "0.12.6";
+  public static String date = "26th April 2025";
 
   private List<NationRestriction> restrictions;
 
