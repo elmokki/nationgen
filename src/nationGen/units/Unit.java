08be956cf2b97da8d0e0e0966843b961e23d008b
Drithyl <hugo.bujan@gmail.com>
2025-05-20 13:50:58 +0200

    Removed unused import

--- src/nationGen/units/Unit.java
+++ src/nationGen/units/Unit.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
